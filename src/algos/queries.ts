7312bff9064a48039e6f2dce1d51ea879ff2c5f7
Jason <jasonmarkericson@gmail.com>
2023-08-18 10:40:15 -0700

    Allowing older posts

--- src/algos/queries.ts
+++ src/algos/queries.ts
@@ -1,20 +1,20 @@
 // Queries used by BlueJ that implement thex various algorithms for fetching posts from the graph.
 
-export const followSimpleQuery =
-    'MATCH (my_person:Person {did: $did })-[:FOLLOW]->(follow_person:Person) ' +
-    'MATCH (follow_person) - [:AUTHOR_OF] -> (post:Post) ' +
-    'WHERE post.indexedAt IS NOT NULL  AND NOT exists((post)-[:PARENT]->(:Post)) ' +
-    'WITH localDateTime() - post.indexedAt as duration, post ' +
-    'WHERE duration.day < 1 AND duration.hour < 12 ' +
-    'RETURN ID(post), post.uri, post.cid, post.repostUri ' +
-    'ORDER BY post.indexedAt DESC ' +
-    'LIMIT 500'
+export const followSimpleQuery = `
+    MATCH (my_person:Person {did: $did })-[:FOLLOW]->(follow_person:Person)
+    MATCH (follow_person) - [:AUTHOR_OF] -> (post:Post)
+    WHERE post.indexedAt IS NOT NULL AND NOT exists((post)-[:PARENT]->(:Post)) AND (localDateTime() - post.indexedAt).second < 60 * 60 * 12
+    RETURN ID(post), post.uri, post.cid, post.repostUri
+    ORDER BY post.indexedAt DESC
+    LIMIT 500
+`
 
-export const clearOldPostsQuery =
-    'MATCH (post:Post) ' +
-    'WITH localDateTime() - post.indexedAt as duration, post ' +
-    'WHERE duration.second > 60 * 60 * 12 ' +
-    'DETACH DELETE post '
+export const clearOldPostsQuery = `
+    MATCH (post:Post)
+    WITH localDateTime() - post.indexedAt as duration, post
+    WHERE duration.second > 60 * 60 * 48
+    DETACH DELETE post
+`
 
 export const postsFromTopEightQuery = `
     MATCH (p1:Person {did: $did })-[i:INTERACTION]->(p2:Person)
